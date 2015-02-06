8ce8f5e465334b49a36a5cf30903bb76e93c4bcc
Matteo Bruni <matteo.bruni@gmail.com>
2015-02-06 12:43:23 +0100

    info stereo

--- main.py
+++ main.py
@@ -75,8 +75,14 @@ def getDisparity(imgLeft, imgRight, method="BM"):
     return disparity_visual
 
 
-imgLeft = (cv2.imread('dataset_templeRing/templeR0001.png'))
-imgRight = (cv2.imread('dataset_templeRing/templeR0002.png'))
+# Choose images
+left_img_number = "01"
+right_img_number = "05"
+
+imgLeft = (cv2.imread('dataset_templeRing/templeR00%s.png' % left_img_number))
+imgRight = (cv2.imread('dataset_templeRing/templeR00%s.png' % right_img_number))
+height, width, depth = imgLeft.shape
+
 
 try:
     method = sys.argv[3]
@@ -104,23 +110,32 @@ for line in parameter_file:
 parametri = np.array(parametri)
 parametri = np.delete(parametri, (0), axis=0)  #remove useless header
 
-R,T,d1,d2, r_left, r_right = get_RT(parametri, 1, 2)
 
 
-height, width, depth = imgLeft.shape
+# Get Rotation Matrix and T of right images from the left one
+# r_left and r_right are the original rotation matrix
+R, T, d1, d2, r_left, r_right = get_RT(parametri, int(left_img_number), int(right_img_number))
+
+# Compute stereo Rectification
 R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(K, d1, K, d2, (height, width), R, T, alpha=0)
 
+# Get map rectification
 map_left1, map_left2 = cv2.initUndistortRectifyMap(K, d1, R1, P1, (height, width), 5)
 map_right1, map_right2 = cv2.initUndistortRectifyMap(K, d1, R2, P2, (height, width), 5)
-
+# Apply Rectification
 left_rectified = cv2.remap(imgLeft, map_left1, map_left2, cv2.INTER_CUBIC)
 right_rectified = cv2.remap(imgRight, map_right1, map_right2, cv2.INTER_CUBIC)
 
+
+# Show images
 cv2.imshow("left", left_rectified)
 cv2.imshow("right", left_rectified)
 cv2.waitKey(0)
 
 # cv2.reprojectImageTo3D()
+
+
+
 # #templeR0002.png 1520.400000 0.000000 302.320000 0.000000 1525.900000 246.870000 0.000000 0.000000 1.000000 0.00272557078828676410 0.98353557606148900000 -0.18069405603193772000 0.99651741905514424000 -0.01773058775937118300 -0.08147797111723514800 -0.08334029507718225500 -0.17984270037758626000 -0.98015865977776562000 -0.0288222339759 -0.0306361018019 0.525505113107
 # #"imgname.png k11 k12 k13 k21 k22 k23 k31 k32 k33 r11 r12 r13 r21 r22 r23 r31 r32 r33 t1 t2 t3"
 # #The projection matrix for that image is given by K*[R t]
